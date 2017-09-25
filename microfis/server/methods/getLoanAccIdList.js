3c5efe0367e54e06621270b9defcd70dc2c04937
Narong <narongsao98@gmail.com>
2017-09-25 15:56:47 +0700

    kk

---
+++ microfis/server/methods/getLoanAccIdList.js
@@ -0,0 +1,44 @@
+import {check} from 'meteor/check';
+import math from 'mathjs';
+import {Repayment} from '../../common/collections/repayment.js';
+Meteor.methods({
+    microfis_getloanAccIdList: function (detailPaid, closeDate) {
+        let repaymentIdList = [];
+        let loanIdList = [];
+
+        if (detailPaid.length > 0) {
+            repaymentIdList = detailPaid.map(function (obj) {
+                return obj.repaymentId;
+            })
+        }
+        loanIdList = Repayment.find({
+            _id: {$in: repaymentIdList},
+            //repaidDate: {$gte: moment(closeDate).startOf("day").toDate()}
+        }).map(function (obj) {
+            return obj.loanAccId;
+        })
+        return loanIdList;
+    },
+    microfis_checkRepaymentExistOfClearPrepay: function (detailPaid, closeDate) {
+        let repaymentIdList = [];
+        let loanIdList = [];
+
+        if (detailPaid.length > 0) {
+            repaymentIdList = detailPaid.map(function (obj) {
+                return obj.repaymentId;
+            })
+        }
+        loanIdList = Repayment.find({
+            _id: {$in: repaymentIdList}
+        }).map(function (obj) {
+            return obj.loanAccId;
+        })
+
+        let repaidList = Repayment.find({
+            loanAccId: {$in: loanIdList},
+            repaidDate: {$gte: moment(closeDate).startOf("day").toDate()}
+        }).fetch();
+
+        return repaidList.length;
+    }
+})
