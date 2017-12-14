4917825c92c30dd6a667ea1259670503d5039457
Narong <narongsao98@gmail.com>
2017-12-14 11:36:24 +0700

    report audit

--- microfis/client/routes-report.js
+++ microfis/client/routes-report.js
@@ -494,3 +494,19 @@ MicrofisRoutes.route('/inventory-report', {
         parent: 'microfis.home'
     }
 });
+// Audit
+import '../imports/reports/audit';
+MicrofisRoutes.route('/audit-report', {
+    name: 'microfis.auditReport',
+    title: 'Audit Report',
+    action: function (params, queryParams) {
+        Layout.main('Microfis_auditReport');
+    },
+    breadcrumb: {
+        //params: ['id'],
+        //queryParams: ['show', 'color'],
+        title: 'Audit Report',
+        icon: 'fa-circle-o',
+        parent: 'microfis.home'
+    }
+});
